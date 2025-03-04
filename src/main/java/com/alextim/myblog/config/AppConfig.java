0250fa718ebc459a10f14ab58e1da5f9c67b7c0e
Aleksey Timofeev <atimofeev1508@gmail.com>
2025-03-05 01:01:03 +0300

    change config files

--- src/main/java/com/alextim/myblog/config/AppConfig.java
+++ src/main/java/com/alextim/myblog/config/AppConfig.java
@@ -1,15 +1,11 @@
-package com.alextim.myblog;
+package com.alextim.myblog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-//@Profile("prod123")
 @ComponentScan("com.alextim.myblog")
 @PropertySource("classpath:application.properties")
-@EnableWebMvc
 public class AppConfig {
 }
--- src/test/java/com/alextim/myblog/config/AppTestConfig.java
+++ src/test/java/com/alextim/myblog/config/AppTestConfig.java
@@ -2,12 +2,18 @@ package com.alextim.myblog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ActiveProfiles;
 
 @Configuration
 @ActiveProfiles("test")
-@ComponentScan("com.alextim.myblog")
+@ComponentScan(
+        basePackages = {"com.alextim.myblog"},
+        excludeFilters = {
+            @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, value=AppConfig.class)
+        }
+)
 @PropertySource("classpath:application-test.properties")
 public class AppTestConfig {
 }
