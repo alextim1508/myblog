a6fb9b3c53cd221cb4c3605cdd05856bf76db521
Aleksey Timofeev <atimofeev1508@gmail.com>
2025-03-10 23:59:51 +0300

    modification config

--- src/main/java/com/alextim/myblog/config/DataSourceConfig.java
+++ src/main/java/com/alextim/myblog/config/DataSourceConfig.java
@@ -1,24 +1,15 @@
 package com.alextim.myblog.config;
 
-import jakarta.persistence.EntityManagerFactory;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories("com.alextim.myblog.repository")
 public class DataSourceConfig {
 
     @Bean
@@ -36,50 +27,16 @@ public class DataSourceConfig {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-            DataSource dataSource,
-            @Value("${hibernate.dialect}") String hibernateDialect,
-            @Value("${hibernate.hbm2ddl.auto}") String hbm2ddlAuto,
-            @Value("${hibernate.show_sql}") String showSql,
-            @Value("${hibernate.format_sql}") String formatSql) {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setPackagesToScan("com.alextim.myblog.model");
-        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", hibernateDialect);
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
-        hibernateProperties.setProperty("hibernate.show_sql", showSql);
-        hibernateProperties.setProperty("hibernate.format_sql", formatSql);
-        factoryBean.setJpaProperties(hibernateProperties);
-
-        return factoryBean;
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
     @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
-
-    @Bean
-    @Profile("prod")
     public Flyway flyway(DataSource dataSource) {
         Flyway flyway = Flyway.configure()
                 .dataSource(dataSource)
                 .load();
         flyway.migrate();
         return flyway;
-    }
-
-    @Bean
-    @Profile("test")
-    public Flyway flywayForTests(DataSource dataSource) {
-        return Flyway.configure()
-                .baselineOnMigrate(true)
-                .dataSource(dataSource)
-                .load();
     }
 }
